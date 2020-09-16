ca47e325df0cfbd70156376661bb76679ffc8db4
Mustafa Hamraz <mustafa-hamraz@chasacademy.se>
2020-09-17 00:20:25 +0200

    Menu has been done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'about', component: AboutComponent }, 
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
